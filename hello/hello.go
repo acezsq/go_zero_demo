48920c352badbb00e4e651a82c3fc57178e14e8e
zxs-000202 <2635718400@qq.com>
2024-03-05 12:12:39 +0800

    快速开发单体应用

---
+++ hello/hello.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"go_zero_demo/hello/internal/config"
+	"go_zero_demo/hello/internal/handler"
+	"go_zero_demo/hello/internal/svc"
+
+	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/rest"
+)
+
+var configFile = flag.String("f", "etc/hello-api.yaml", "the config file")
+
+func main() {
+	flag.Parse()
+
+	var c config.Config
+	conf.MustLoad(*configFile, &c)
+
+	server := rest.MustNewServer(c.RestConf)
+	defer server.Stop()
+
+	ctx := svc.NewServiceContext(c)
+	handler.RegisterHandlers(server, ctx)
+
+	fmt.Printf("Starting server at %s:%d...\n", c.Host, c.Port)
+	server.Start()
+}
